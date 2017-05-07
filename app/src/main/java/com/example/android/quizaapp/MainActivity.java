b69bffe692dd844590c2f09f182f4ae49cccb2af
JS IID <haseebb.kithran@gmail.com>
2017-05-08 01:43:03 +0500

    yes

--- app/src/main/java/com/example/android/quizaapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizaapp/MainActivity.java
@@ -11,110 +11,114 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import static com.example.android.quizaapp.R.id.Answer_1;
+import static com.example.android.quizaapp.R.id.EntryTxt;
 
 public class MainActivity extends AppCompatActivity {
-   public CheckBox Ans1,Ans2,Ans3;
-    private EditText Name;
+
     int score = 0;
-    public boolean isChoiceAns1 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Ans1  = (CheckBox) findViewById(Answer_1);
-        Ans2 = (CheckBox) findViewById(R.id.Answer_2);
-        Ans3 = (CheckBox) findViewById(R.id.Answer_3);
-        Name  = (EditText) findViewById(R.id.Name);
-
     }
-    public void checkResult(View v) {
+    public void submitResult(View v) {
+        CheckBox Answer1CheckBox = (CheckBox) findViewById(Answer_1);
+        boolean Answer1 = Answer1CheckBox.isChecked();
+        CheckBox Answer4CheckBox = (CheckBox) findViewById(R.id.Answer_4);
+        boolean Answer4 = Answer4CheckBox.isChecked();
+        EditText editText = (EditText) findViewById(EntryTxt);
+        String EntryAns = editText.getText().toString();
 
-        RadioButton rb2;
-        RadioButton rb3;
-        RadioButton rb4;
-        RadioButton rb5;
-        RadioButton rb6;
-        RadioButton rb7;
-        RadioButton rb8;
-        RadioButton rb9;
-        RadioButton rb10;
-
-        rb2 = (RadioButton) findViewById(R.id.A2);
-        rb3 = (RadioButton) findViewById(R.id.A3);
-        rb4 = (RadioButton) findViewById(R.id.A4);
-        rb5 = (RadioButton) findViewById(R.id.A5);
-        rb6 = (RadioButton) findViewById(R.id.A6);
-        rb7 = (RadioButton) findViewById(R.id.A7);
-        rb8 = (RadioButton) findViewById(R.id.A8);
-        rb9 = (RadioButton) findViewById(R.id.A9);
-        rb10 = (RadioButton) findViewById(R.id.A10);
-        if (rb2.isChecked()) {
-            score = score + 1;
-        }
-        if (rb3.isChecked()) {
-            score = score + 1;
-        }
-        if (rb4.isChecked()) {
-            score = score + 1;
-        }
-        if (rb5.isChecked()) {
-            score = score + 1;
-        }
-        if (rb6.isChecked()) {
-            score = score + 1;
-        }
-        if (rb7.isChecked()) {
-            score = score + 1;
-        }
-        if (rb8.isChecked()) {
-            score = score + 1;
-        }
-        if (rb9.isChecked()) {
-            score = score + 1;
-        }
-        if (rb10.isChecked()) {
-            score = score + 1;
-        }
-        onCheckboxClicked();
+//
+    score = Calculatescore(Answer1,Answer4,EntryAns);
         displayScore(score);
         score = 0;
     }
-    public void onCheckboxClicked(View view) {
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
+  private int Calculatescore(boolean HasAns1,boolean HasAns4,String EntryAns){
+      RadioButton rb2;
+      RadioButton rb3;
+      RadioButton rb4;
+      RadioButton rb5;
+      RadioButton rb6;
+      RadioButton rb7;
+      RadioButton rb8;
+      RadioButton rb9;
+      RadioButton rb10;
 
-        // Check which checkbox was clicked
-        switch(view.getId()) {
-            case Answer_1:
-                if (checked)
-                isChoiceAns1 = true;
-                 score = score+1;
-                break;
-            case R.id.Answer_2:
-                if (checked)
-                isChoiceAns1 = false;
-                break;
-            case R.id.Answer_3:
-                if (checked)
-                    isChoiceAns1 = false;
+      rb2 = (RadioButton) findViewById(R.id.A2);
+      rb3 = (RadioButton) findViewById(R.id.A3);
+      rb4 = (RadioButton) findViewById(R.id.A4);
+      rb5 = (RadioButton) findViewById(R.id.A5);
+      rb6 = (RadioButton) findViewById(R.id.A6);
+      rb7 = (RadioButton) findViewById(R.id.A7);
+      rb8 = (RadioButton) findViewById(R.id.A8);
+      rb9 = (RadioButton) findViewById(R.id.A9);
+      rb10 = (RadioButton) findViewById(R.id.A10);
+     int score = 0;
+      if (rb2.isChecked()) {
+          score = score + 1;
+      }
+      if (rb3.isChecked()) {
+          score = score + 1;
+      }
+      if (rb4.isChecked()) {
+          score = score + 1;
+      }
+      if (rb5.isChecked()) {
+          score = score + 1;
+      }
+      if (rb6.isChecked()) {
+          score = score + 1;
+      }
+      if (rb7.isChecked()) {
+          score = score + 1;
+      }
+      if (rb8.isChecked()) {
+          score = score + 1;
+      }
+      if (rb9.isChecked()) {
+          score = score + 1;
+      }
+      if (rb10.isChecked()) {
+          score = score + 1;
 
-        }
-    }
+      }
+      if (HasAns1 && HasAns4){
+          score = score + 1;
+      }
+      else if(HasAns1){
+          score = score+1;
+      }
+      else if (HasAns4){
+          score = score +1;
+      }
+
+
+      if (EntryAns == "Venus" ){
+          score = score +1;
+      }
+      if (EntryAns == "venus"){
+          score = score + 1;
+      }
+
+
+    return score;
+  }
 
     private void displayScore(int number) {
         Context context = getApplicationContext();
-        CharSequence text =  Name +"Scored "+number +" out of 10";
+        CharSequence text = "you Scored "+number +" out of 11";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
     }
 
-    public void onClick(View v) {
+
+    public void Reset(View v) {
         score = 0;
-        displayScore(score);
+        EditText editText = (EditText) findViewById(EntryTxt);
         RadioGroup q2radiobox = (RadioGroup) findViewById(R.id.q2radiobox);
         q2radiobox.clearCheck();
         RadioGroup q3radiobox = (RadioGroup) findViewById(R.id.q3radiobox);
@@ -133,6 +137,6 @@ public class MainActivity extends AppCompatActivity {
         q9radiobox.clearCheck();
         RadioGroup q10radiobox = (RadioGroup) findViewById(R.id.q10radiobox);
         q10radiobox.clearCheck();
-
+        editText.setText("");
     }
 }
